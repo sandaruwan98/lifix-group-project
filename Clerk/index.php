8dcd7c0b130e93b40905ebdd496a80b6fcddfdb7
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-09-30 12:23:41 +0530

    purchase layout

--- Clerk/index.php
+++ Clerk/index.php
@@ -22,7 +22,7 @@
             </li>
             <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-home"></i><span class="link-text">Home</span> </a></li>
             <li class="nav-item"><a class="nav-link active" href="#"><i class="fas fa-columns"></i><span class="link-text">DailyRepairs</span> </a></li>
-            <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-history"></i><span class="link-text">RepairHistory</span></a></li>
+            <li class="nav-item"><a class="nav-link" href="./repairHistory.html"><i class="fas fa-history"></i><span class="link-text">RepairHistory</span></a></li>
             <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-file-invoice"></i><span class="link-text">Purchases</span></a></li>
             <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-plus-square"></i><span class="link-text">LampPost</span></a></li>
             <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-cog"></i><span class="link-text">Settings</span></a></li>
