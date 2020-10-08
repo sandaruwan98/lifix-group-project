ce820ae860870bb157ac97bd7caccac5f101fe6b
Gajani1998 <gajanikangesan@gmail.com>
2020-10-08 15:44:01 +0530

    modified the insert file

--- storekeeper/insert.php
+++ storekeeper/insert.php
@@ -3,10 +3,13 @@ $conn=new PDO("mysql:host=localhost;dbname=lifix","root","");
 if(isset($_POST['save'])){	
 $lp_id=$_POST['lp_id'];
 $bulb=$_POST['bulb'];
-$wires=$_POST['wires'];
+$sunbox=$_POST['sunbox'];
+$wires=$_POST['wire'];
 $switch=$_POST['switch'];
 $holder=$_POST['holder'];
-$save=$conn->prepare("insert into issue_item(lp_id,date,bulb,wires,switch,holder)values('$lp_id',curdate(),'$bulb','$wires','$switch','$holder')");
+$screw_holder=$_POST['sholder'];
+$pin_holder=$_POST['pin_holder'];
+$save=$conn->prepare("insert into issue_item(lp_id,date,bulb,sunbox,wire,switch,Holder,Screw_holder,3_pin_holder)values('$lp_id',curdate(),'$bulb','$sunbox','$wires','$switch','$holder','$screw_holder','$pin_holder')");
 	
 	if($save->execute()){
 		
