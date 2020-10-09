d7d14bd6d4d1b9158b950e5257abde6d9bbd9dc7
UdithaIshan <uditha.ishan34@gmail.com>
2020-10-09 22:53:43 +0530

    DSec updated

--- DivisionalSecretary/userCtrl.php
+++ DivisionalSecretary/userCtrl.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>User Control</title>
     <script src="https://kit.fontawesome.com/2b554022ef.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../css/slider.css">
     <link rel="stylesheet" href="style.css">
