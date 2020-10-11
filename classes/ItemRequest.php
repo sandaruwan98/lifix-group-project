578ce2ebbc82c0b1b2e9bc8fe25fdeff71b799d2
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-10-11 12:08:08 +0530

    item request for tecnician

--- classes/ItemRequest.php
+++ classes/ItemRequest.php
@@ -5,15 +5,13 @@ class ItemRequest extends Database
 {
 
    
-    public function AddRequestItemRecord($tecnnician_id){
+    public function AddRequestItemRecord($created_user_id,$storekeeper_id,$status){
         $q = "INSERT INTO `itemrequest`( `status`, `completed_by`, `created_by`) VALUES 
-        ('o', 0 , '$tecnnician_id')";
+        ('$status', '$storekeeper_id' , '$created_user_id')";
 
         $this->conn->query($q);
     }
    
-
-
     public function AddRequestedItemAsc($req_id,$item_id,$quantity){
         $q = "INSERT INTO `itemrequest_inventory_asc`( `Itemrequest_id`, `item_id`, `quantity`) VALUES 
         ('$req_id','$item_id' , '$quantity')";
@@ -23,13 +21,17 @@ class ItemRequest extends Database
 
 
 
-    
-    public function CreateItemRequest($req_id,$tecnnician_id,$request_items){
-        $this->AddRequestItemRecord($tecnnician_id);
 
+
+
+    
+    public function CreateItemRequest($created_user_id,$request_items){
+        // 
+        $this->AddRequestItemRecord($created_user_id, 0 ,'o');
+        $last_id = $this->conn->insert_id;
         // add ussed items to database
         foreach ($request_items as $item){
-            $this->AddRequestedItemAsc($req_id, $item[0],$item[1]);
+            $this->AddRequestedItemAsc($last_id, $item[0],$item[1]);
         }
         
     }
