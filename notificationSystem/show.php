a1fc85b5a2c843d92c56ceeb5870a8bc3736924d
UdithaIshan <uditha.ishan34@gmail.com>
2020-11-14 18:34:01 +0530

    notification system

---
+++ notificationSystem/show.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+/* Style The Dropdown Button */
+.dropbtn {
+  background-color: #4CAF50;
+  color: white;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+}
+
+/* The container <div> - needed to position the dropdown content */
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+/* Dropdown Content (Hidden by Default) */
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+/* Change color of dropdown links on hover */
+.dropdown-content a:hover {background-color: #f1f1f1}
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+/* Change the background color of the dropdown button when the dropdown content is shown */
+.dropdown:hover .dropbtn {
+  background-color: #3e8e41;
+}
+</style>
+</head>
+<body>
+<div class="dropdown">
+  <button class="dropbtn"><span class="count"></span>Dropdown</button>
+  <div class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+</div>
+
+<form action="" method="post">
+    <input type="text" name="comment" id="comment">
+    <input type="submit" value="submit">
+
+</form>
+
+<script>
+    $(document).ready(function(){
+        function loadUnseenNotifications(view = "") {
+            $.ajax({
+                url : "fetch.php",
+                method : "POST",
+                data : {view:view},
+                dataType : "JSON",
+                success : function(data) {
+                    $('.dropdown-content').html(data.notification);
+                    
+                }
+            })
+        }
+    });
+</script>
+</body>
+</html>
