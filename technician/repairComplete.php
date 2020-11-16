34127b29242fe5e3343fb9e847c9612faf13f9b2
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-11-16 22:37:15 +0530

    nav technitian

--- technician/repairComplete.php
+++ technician/repairComplete.php
@@ -67,30 +67,12 @@ if (isset($_POST["complete"]) ) {
 </head>
 
 <body>
-    <nav class="sidebar">
-        <!-- <h2 class="link-text">MENU</h2> -->
-        <ul>
-            <li class="nav-logo"><span class="nav-link" href="#"><i class="fas fa-lightbulb"></i><span class="link-text"
-                        style="margin-left: 5px;">LiFix</span></span></li>
-            <li class="nav-item"><a class="nav-link " href="./index.php"><i class="fas fa-home"></i><span
-                        class="link-text">Home</span> </a></li>
-            <li class="nav-item"><a class="nav-link " href="./map.html"><i class="fas fa-map"></i><span
-                        class="link-text">ViewMap</span> </a></li>
-            <li class="nav-item"><a class="nav-link" href="./request.php"><i
-                        class="fas fa-plus-square"></i><span class="link-text ">Request</span></a></li>
-            <li class="nav-item"><a class="nav-link" href="./EmgRepair.php"><i class="fas fa-exclamation-circle"></i><span
-                        class="link-text">EmgRepair</span></a></li>
-            <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-shower"></i><span
-                        class="link-text">Lamppost</span></a></li>
-
-        </ul>
-
-    </nav>
-
-    <script src="../js/slider.js"></script>
 
 
-    <div class="main">
+<?php include './nav.html' ?>
+
+
+<div class="main">
         <div class="con">
 
 
