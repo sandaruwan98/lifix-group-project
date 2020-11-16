ca33e2feb2e398ce5dcacea1ab877d248ad72a80
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-11-17 00:35:14 +0530

    request page

--- technician/requestlist.php
+++ technician/requestlist.php
@@ -30,28 +30,23 @@ $requestlist = $itemrequest->getPendingRequestList_by_userid(1);
 
     <div class="main">
         <div class="con">
-            <h2 class="title-r">Pending Item Requsts</h2>
-            <div class="list">
-                <?php while ($row = $requestlist->fetch_assoc()) :
-                ?>
-
-                <div class="list-item" id="<?= $row["Itemrequest_id"] ?>">
-                    
-                        <div class="row">
-                            <div class="id"># <?= $row['Itemrequest_id'] ?> </div>
-                            <div class="date"><?= $row["added_date"] ?></div>
-                        </div>
-
-                        
-                    
-                    <!-- <button onclick="location.href='./repairComplete.php?id=<?= $row["repair_id"] ?> ';" id="btnComplete" class="btn"> <i class="s fas fa-check"></i></button> -->
-                </div>
-
-                <?php endwhile ?>
 
 
-            </div>
+            <?php
+            
+            if(isset($_GET["id"])){
+                include './views/requestpage.php';
+            }else{
+                include './views/pendingRequestList.php';
+            }
 
+            ?>
+                
+                   
+
+
+
+            
         </div>
     </div>
 </body>
