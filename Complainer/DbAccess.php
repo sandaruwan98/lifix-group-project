e9447fa5d1c1a5221ccfad19f3cbe24f134d9912
Gajani1998 <56734514+Gajani1998@users.noreply.github.com>
2020-11-20 12:13:29 +0530

    Add files via upload

---
+++ Complainer/DbAccess.php
@@ -0,0 +1,71 @@
+<?php 
+
+    include_once '../utils/classloader.php';
+
+    $errors = array('name'=>'', 'nic'=>'', 'lampid'=>'', 'phone'=>'', 'otp'=>'');
+    $name = $nic = $lampId = $phoneNo = $otpCode = $note = "";
+
+    class DbAccess {
+        public $repairObj;
+        public $complaintObj;
+        
+        public function sendData($page) {
+            
+            $repairObj = new classes\Repair();
+            $complaintObj = new classes\Complaint();
+
+            $name = $_POST['name'];
+            $nic = $_POST['nic'];
+            $lp_id = $_POST['lampid'];
+            $phoneno = $_POST['phone'];
+            $note = $_POST['note'];
+            array_search('yes', $_POST)?($bulb = 1):($bulb = 0);
+
+            $repairId = $repairObj->createRepair('a', $lp_id, 0, 0);
+            $complainerCheck = $complaintObj->checkComplainerExists($nic);
+            $complainer_id = $complaintObj->addComplainer($complainerCheck, $nic, $name, $phoneno);
+            $complaintObj->addComplaint($bulb, $note, $lp_id, $repairId, $complainer_id);
+
+            if ($complainer_id &&  $repairId) {
+                header("location: $page");
+            }
+        }
+    }
+
+	if(isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $nic = $_POST['nic'];
+        $lampId = $_POST['lampid'];
+        $phoneNo = $_POST['phone'];
+        $otpCode = $_POST['otp'];
+        $note = $_POST['note'];
+
+        if(empty($name) || !preg_match("/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/", $name)) {
+            $name = "";
+            $errors['name'] = 'Name must be a valid name';
+        }
+        if(empty($nic) || !preg_match("/^[1-9]([0-9]{8}(x|v|X|V)|[0-9]{11})$/", $nic)) {
+            $nic = "";
+            $errors['nic'] = 'NIC must be a valid NIC number';
+        }
+        if(empty($lampId) || !preg_match("/^[1-9][0-9]{3}$/", $lampId)) {
+            $lampId = "";
+            $errors['lampid'] = 'Lamp post ID must be a valid ID';
+        }
+        // if(empty($phoneNo || !preg_match('((\+94)|0)[0-9]{2}[.\- ]?[0-9]{3}[.\- ]?[0-9]{4}', $phoneNo))) {
+        //     echo "fdd";//$errors['phone'] = 'Lamp post ID must be a valid ID number';
+        // }
+        if(empty($phoneNo)) {
+            $phoneNo = "";
+            $errors['phone'] = 'Enter valid number';
+        }
+        if(empty($otpCode)) {
+            $errors['otp'] = 'OTP must be a valid number';
+        }
+
+        if(!array_filter($errors)) {
+            $obj = new DbAccess();
+            $obj->sendData($page);
+        }
+    }
+?>
---
+++ Complainer/tamil.php
@@ -0,0 +1,53 @@
+<?php
+	$page = 'tamil.php';
+	include "DbAccess.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Li - Fix</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0">
+</head>
+<body>
+	<div class="container">
+	<section>
+		<a id="lan" class="floating-btn">த</a>
+	</section>
+		<div class="contact-box">
+			<div class="left"></div>
+			<div class="right">
+				<h2>முறைப்பாடு</h2>
+				<form action="tamil.php" method="POST">
+					<input type="text" name="name" value="<?php echo htmlspecialchars($name) ?>" class="field" placeholder="<?php if($errors['name']) echo 'பெயர் சரியான பெயராக இருக்க வேண்டும்'; else echo 'உங்களது பெயர்'; ?>" id="f1">
+					<input type="text" name="nic" value="<?php echo htmlspecialchars($nic) ?>" class="field" placeholder="<?php if($errors['nic']) echo 'என்ஐசி செல்லுபடியாகும் என்ஐசி எண்ணாக இருக்க வேண்டும்'; else echo 'அடையாள அட்டை இல'; ?> " id="f2">
+					<!-- <input type="text" name="lampid" value="<?php echo htmlspecialchars($lampId) ?>" class="field" placeholder="<?php if($errors['lampid']) echo 'சரியான அடையாள எண்ணாக இருக்க வேண்டும்'; else echo 'மின்விளக்கு கம்ப இல'; ?>" id="f3"> -->
+					<div class="box">
+						<div class="p-left">
+							<input type="text" name="lampid" value="<?php echo htmlspecialchars($lampId) ?>" class="field" placeholder="<?php if($errors['lampid']) echo 'சரியான அடையாள எண்ணாக இருக்க வேண்டும்'; else echo 'மின்விளக்கு கம்ப இல'; ?>" >					
+						</div>
+						<div class="bulb">
+							<span><label for="bulb">விளக்கை? </label><input class="checkmark" type="checkbox" name="bulb" id="bulb" value="yes"></span>
+						</div>
+					</div>
+					<input type="text" name="note" value="<?php echo htmlspecialchars($note) ?>" class="field note" placeholder="பிரச்சனை குறிப்பு" id="f4">
+					<div class="box">
+						<div class="p-left">
+							<input type="text" name="phone" value="<?php echo htmlspecialchars($phoneNo) ?>" class="field" placeholder="<?php if($errors['phone']) echo 'சரியான எண்ணை உள்ளிடவும்'; else echo 'தொலைபேசி'; ?>" id="f5">
+						</div>
+						<div class="p-right">
+							<button class="btn2">குறியீடு</button>
+						</div>
+					</div>
+					<input type="text" name="otp" class="field" value="<?php echo htmlspecialchars($otpCode) ?>" placeholder="<?php if($errors['otp']) echo 'OTP சரியான  இருக்க வேண்டும்'; else echo 'OTP குறியீடு'; ?>" id="f6">
+					<button name="submit" class="btn">SUBMIT</button>
+				</form>
+			</div>
+		</div>
+	</div>
+	<script src="index.js"></script>
+</body>
+</html>
