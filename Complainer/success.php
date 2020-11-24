8426da39ab2d89d020809508932355e13c8f209f
UdithaIshan <uditha.ishan34@gmail.com>
2020-11-24 05:50:29 +0530

    cleaned & updated

--- Complainer/success.php
+++ Complainer/success.php
@@ -6,14 +6,14 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="success.css">
+    <link rel="stylesheet" href="css/success.css">
     <title>Li - Fix</title>
 </head>
     <body>
         <div class="outer">
             <div class="middle">
                 <div class="inner">
-                    <img src="img/4.svg" alt="checkmark">
+                    <img src="img/3.svg" alt="checkmark">
                     <h1><?php echo $_SESSION['h1'];?></h1>
                     <p><?php echo $_SESSION['p'];?></p>
                     <a href="<?php echo $_SESSION['page'];?>"><button class="btn"><?php echo $_SESSION['btn'];?></button></a>
--- Complainer/tamil.php
+++ Complainer/tamil.php
@@ -10,7 +10,7 @@
 <html>
 <head>
 	<title>Li - Fix</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0">
