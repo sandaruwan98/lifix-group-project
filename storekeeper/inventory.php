aae8bef6179510f9f2c4615130d0e0ce15b982c0
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-11-27 09:38:45 +0530

    inventory page st

--- storekeeper/inventory.php
+++ storekeeper/inventory.php
@@ -14,7 +14,6 @@ $session = new classes\Session(StorekeeperFL);
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="./store.css">
     <link rel="stylesheet" href="./display.css">
-    <link rel="stylesheet" href="../clerk/css/repairHistory.css">
     <script src="https://kit.fontawesome.com/2b554022ef.js" crossorigin="anonymous"></script>
     <title>Inventory Records</title>
 
@@ -25,45 +24,97 @@ $session = new classes\Session(StorekeeperFL);
     <?php include "./views/nav.php" ?>
 
 
+<div class="main_content">
+
+
     
-<div class="frow">
-     <h1>Inventory Details</h1>
-  </div>
-            
-<table class="ctable">
-<thead>                 
-<div id="" class="repair-item">
-     <div class="row">
+    <div class="frow">
+        <h1>Inventory Details</h1>
+    </div>
+    <div class="con">
+        
+        <h1 class="heading" style="margin-left: 2.5%;margin-right: 2.5%;">Inventory Balance</h1>
+        <table class="content-table ctable">
+        <thead> 
+            <tr>
+            <th>Item Id</th>
+            <!-- <th><span>Date</span></th> -->
+            <th><span>Name</span></th>
+            <th><span>Current Balance</span></th>
 
-<tr>
-<th>Item Id</th>
-<!-- <th><span>Date</span></th> -->
-<th><span>Name</span></th>
-<th><span>Total</span></th>
+            </tr>
+            </div>
+            </div>
+        </thead>
 
-</tr>
-</div>
- </div>
-</thead>
-
-<?php
-
-
-$inventory = new models\Inventory();
-$result = $inventory->getAllInventory();
+        <?php
 
 
-while($row=mysqli_fetch_assoc($result)){
-    echo"<tr>";
-    echo "<td>".$row['Item_id']."</td>";
-    // echo "<td>".$row['date']."</td>";
-    echo "<td>".$row['name']."</td>";
-    echo "<td>".$row['total']."</td>";
-    echo"</tr>";
-}
+        $inventory = new models\Inventory();
+        $result = $inventory->getAllInventory();
 
-?>
-</table>
+
+        while($row=mysqli_fetch_assoc($result)){
+            echo"<tr>";
+            echo "<td>".$row['Item_id']."</td>";
+            // echo "<td>".$row['date']."</td>";
+            echo "<td>".$row['name']."</td>";
+            echo "<td>".$row['total']."</td>";
+            echo"</tr>";
+        }
+
+        ?>
+        </table>
+    </div>
+
+
+
+    <div class="con">
+        <div class="new-stock">
+            <h1 class="heading">New Stocks</h1>
+
+            <!-- Nothing new yet -->
+            <div class="row">
+            <span class="lft">Date : </span>  <span class="right">10-04-2020</span>
+            </div>
+            <!-- <br> -->
+            <div class="row">
+            <span class="lft">Added by : </span> <span class="right">Mr. </span>
+            </div>
+
+            <table class="content-table">
+                <thead> 
+                    <tr>
+                    <th>Item Id</th>
+                    <th><span>Name</span></th>
+                    <th><span>Current Balance</span></th>
+
+                    </tr>
+                    </div>
+                    </div>
+                </thead>
+
+                <?php
+
+
+                $inventory = new models\Inventory();
+                $result = $inventory->getAllInventory();
+
+
+                while($row=mysqli_fetch_assoc($result)){
+                    echo"<tr>";
+                    echo "<td>".$row['Item_id']."</td>";
+                    // echo "<td>".$row['date']."</td>";
+                    echo "<td>".$row['name']."</td>";
+                    echo "<td>".$row['total']."</td>";
+                    echo"</tr>";
+                }
+
+                ?>
+            </table>
+        </div>
+    </div>
+</div>  
 </body>
 </html>
 
