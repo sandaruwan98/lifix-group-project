eab53b5f0b9da2fe44bafd5699808f13a7f5bd6f
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-12-30 22:16:21 +0530

    pagination css

---
+++ classes/Pagination.php
@@ -0,0 +1,18 @@
+<?php 
+namespace classes;
+include_once  __DIR__ . '/../utils/classloader.php';
+
+
+class Pagination {
+   
+
+    public function __construct($perpage,$max) {
+        
+    }
+
+
+
+    
+}
+
+
