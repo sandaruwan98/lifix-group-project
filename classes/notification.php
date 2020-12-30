f553a2b53cafd78e4605e820f56df94a2be2bb99
UdithaIshan <uditha.ishan34@gmail.com>
2020-12-30 21:10:27 +0530

    notificationSystem deleted

--- classes/notification.php
+++ classes/notification.php
@@ -1,4 +1,5 @@
 <link href="https://fonts.googleapis.com/css?family=Hind:700" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <link rel="stylesheet" href="../css/fab.css">
 
 <body style="background-color: rgb(72, 127, 149);">
@@ -22,6 +23,63 @@
     </div>
 
     <script>
+
+        $(document).ready(function(){
+        
+        function load_unseen_notification(view = '')
+        {
+        $.ajax({
+        url:"fetch.php",
+        method:"POST",
+        data:{view:view},
+        dataType:"json",
+        success:function(data)
+        {
+            $('.dropdown-menu').html(data.notification);
+            if(data.unseen_notification > 0)
+            {
+            $('.count').html(data.unseen_notification);
+            }
+        }
+        });
+        }
+        
+        load_unseen_notification();
+        
+        $('#comment_form').on('submit', function(event){
+        event.preventDefault();
+        if($('#subject').val() != '' && $('#comment').val() != '')
+        {
+        var form_data = $(this).serialize();
+        $.ajax({
+            url:"insert.php",
+            method:"POST",
+            data:form_data,
+            success:function(data)
+            {
+            $('#comment_form')[0].reset();
+            load_unseen_notification();
+            }
+        });
+        }
+        else
+        {
+        alert("Both Fields are Required");
+        }
+        });
+        
+        $(document).on('click', '.dropdown-toggle', function(){
+        $('.count').html('');
+        load_unseen_notification('yes');
+        });
+        
+        setInterval(function(){ 
+        load_unseen_notification();; 
+        }, 5000);
+        
+        });
+
+
         const fab = document.querySelector('.notification-fab');
         fab.addEventListener('click', () => {
 
