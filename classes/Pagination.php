d35b40464c7aa3cb622c8878fbb8be9aa31062d0
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-12-31 11:40:32 +0530

    pagination frontend part

--- classes/Pagination.php
+++ classes/Pagination.php
@@ -5,14 +5,31 @@ include_once  __DIR__ . '/../utils/classloader.php';
 
 class Pagination {
    
-
-    public function __construct($perpage,$max) {
-        
+    
+    private $perpage;
+    private $max;
+    private $links;
+    
+    public function __construct(int $perpage,int $max,$links=5) {
+        $this->perpage = $perpage;  
+        $this->max = $max;  
+        $this->links = $links;  
     }
 
-
-
-    
+    public function getpageArr(int $page_no)
+    {
+        if ($this->links > $this->max) {
+                return range(1, $this->max);
+        }else {
+            if ($page_no < $this->links-1) {
+                return range(1,$this->links);
+            }elseif( $page_no>($this->max-3) ){
+                return range($this->max - ($this->links-1) , $this->max);
+            }else {
+                return range($page_no-2,$page_no+2);
+            }
+        }
+    }
 }
 
 
--- pagination.php
+++ pagination.php
@@ -1,61 +1,35 @@
-<?php 
+<?php
 
-$maxpages = 10;
+use classes\Pagination;
+
+require './classes/Pagination.php';
+
+$maxpages = 8;
+
+$p = new Pagination(3,$maxpages);
+
 $page_no=$_GET["id"];
-$page_arr=[$page_no-2,$page_no-1,$page_no,$page_no+1,$page_no+2];
+$page_arr=$p->getpageArr($page_no);
 print_r($page_arr);
 ?>
 
-<style>
-    body{
-        background-color: #0e7d8b;
-    }
-    .link {
-        padding: 10px 15px;
-        background: transparent;
-        border:#bccfd8 1px solid;
-        cursor:
-        pointer;
-        color:#bccfd8
-    }
-    .disabled {
-        cursor:not-allowed;
-        color: #bccfd8;
-    }
-    .current {background: #bccfd8;color: #0e7d8b;}
-    .first{border-left:#bccfd8 1px solid;}
-    .pagination{padding-top: 30px;}
-    
-    .dot {
-        padding: 10px 15px; 
-        color: #bccfd8; 
-        font-size: x-large;
-        background: transparent;
-    }
-   </style>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
     <div class="pagination">
     <?php 
         // first
         if ($page_no<4) {   ?>
             
-                <a class="link first" href="<?= $_SERVER["PHP_SELF"]."?id=0" ?>" >&lt;&lt;</a>
-                <a class="link disabled" href="<?= ($page_no > 0) ? $_SERVER["PHP_SELF"]."?id=" . ($page_no+1) : $_SERVER["PHP_SELF"]."?id=" . ($page_no) ?>"   >&lt;</a>
+                <a class="link first" href="<?= $_SERVER["PHP_SELF"]."?id=1" ?>" >&lt;&lt;</a>
+                <a class="link disabled" href="<?= ($page_no > 1) ? $_SERVER["PHP_SELF"]."?id=" . ($page_no-1) : $_SERVER["PHP_SELF"]."?id=" . ($page_no) ?>"   >&lt;</a>
                 <?php foreach ($page_arr as $i) :
                     $class= $i==$page_no ? "current" : ""; ?>
                     <a class="link <?= $class?>" href="<?= $_SERVER["PHP_SELF"]."?id=" . $i ?>" ><?= $i ?></a>
                 <?php endforeach ?>
 
-                <span class="dot">...</span>
-                <a class="link" href="<?= $_SERVER["PHP_SELF"]."?id=" . ($page_no+1) ?>" >&gt;</a>
+                
+                <?= ($maxpages>5) ? '<span class="dot">...</span>' : '' ;?>
+                <a class="link" href="<?= ($page_no < $maxpages) ? $_SERVER["PHP_SELF"]."?id=" . ($page_no+1) : $_SERVER["PHP_SELF"]."?id=" . ($page_no) ?>" >&gt;</a>
                 <a class="link" href="<?= $_SERVER["PHP_SELF"]."?id=" . $maxpages ?>" >&gt;&gt;</a>
             
 
@@ -64,9 +38,9 @@ print_r($page_arr);
         //  last
         }elseif ($page_no>$maxpages-3) {?>
 
-        <a class="link  first" href="<?= $_SERVER["PHP_SELF"]."?id=0" ?>" >&lt;&lt;</a>
+        <a class="link  first" href="<?= $_SERVER["PHP_SELF"]."?id=1" ?>" >&lt;&lt;</a>
         <a class="link " href="<?= $_SERVER["PHP_SELF"]."?id=" . ($page_no-1) ?>" >&lt;</a>
-        <span class="dot">...</span>
+        <?= ($maxpages>5) ? '<span class="dot">...</span>' : '' ;?>
        
         <?php foreach ($page_arr as $i) :
             $class= $i==$page_no ? "current" : ""; ?>
@@ -81,7 +55,7 @@ print_r($page_arr);
         // mid
         }else { ?>
 
-        <a class="link  first" href="<?= $_SERVER["PHP_SELF"]."?id=0" ?>" >&lt;&lt;</a>
+        <a class="link  first" href="<?= $_SERVER["PHP_SELF"]."?id=1" ?>" >&lt;&lt;</a>
         <a class="link "  href="<?= $_SERVER["PHP_SELF"]."?id=" . ($page_no-1) ?>" >&lt;</a>
         <span class="dot">...</span>
        
@@ -104,5 +78,3 @@ print_r($page_arr);
     <div class="pagination">
       
     </div>
-</body>
-</html>
