a953d97436583f06ef9d80d370e54b529b51a639
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-01-06 14:41:21 +0530

    sql

--- lifix.sql
+++ lifix.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 01, 2021 at 06:49 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.4
+-- Generation Time: Jan 06, 2021 at 10:10 AM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -43,7 +44,8 @@ INSERT INTO `complainer` (`complainer_id`, `NIC`, `Name`, `phone_no`) VALUES
 (2, '862171670v', 'Mr. Perera', '0714570342'),
 (3, '22222222222', 'Lakshan Sandaruwan Jayasinghe', '0701549225'),
 (4, '982175745v', 'Lakshan Sandaruwan Jayasinghe', '+94701549225'),
-(5, '652175745v', 'Kumara perera', '+94701549225');
+(5, '652175745v', 'Kumara perera', '+94701549225'),
+(6, '982171777v', 'llllllllllllllllllllllllllllll', '0700003333');
 
 -- --------------------------------------------------------
 
@@ -166,6 +168,18 @@ CREATE TABLE `itemrequest` (
   `supplied_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `itemrequest`
+--
+
+INSERT INTO `itemrequest` (`Itemrequest_id`, `status`, `completed_by`, `created_by`, `added_date`, `supplied_date`) VALUES
+(56, 'a', 0, 32, '2020-12-26', '0000-00-00'),
+(57, 'b', 0, 32, '2020-12-26', '0000-00-00'),
+(58, 'a', 0, 33, '2020-12-26', '0000-00-00'),
+(59, 'b', 0, 32, '2020-12-31', '0000-00-00'),
+(60, 'a', 0, 32, '2020-12-31', '0000-00-00'),
+(61, 'a', 0, 32, '2020-12-31', '0000-00-00');
+
 -- --------------------------------------------------------
 
 --
@@ -177,6 +191,23 @@ CREATE TABLE `itemrequest_inventory_asc` (
   `Item_id` int(255) NOT NULL,
   `quantity` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `itemrequest_inventory_asc`
+--
+
+INSERT INTO `itemrequest_inventory_asc` (`Itemrequest_id`, `Item_id`, `quantity`) VALUES
+(56, 1, 1),
+(56, 3, 5),
+(57, 3, 34),
+(57, 5, 3),
+(58, 1, 44),
+(59, 3, 23),
+(59, 5, 33),
+(60, 8, 56),
+(60, 10, 5),
+(61, 3, 3),
+(61, 5, 45);
 
 -- --------------------------------------------------------
 
@@ -276,14 +307,14 @@ CREATE TABLE `repair` (
 --
 
 INSERT INTO `repair` (`repair_id`, `date`, `status`, `lp_id`, `technician_id`, `clerk_id`) VALUES
-(40, '2020-11-29', 'a', 1000, 0, 0),
+(40, '2020-11-29', 'a', 1000, 33, 0),
 (41, '2020-11-29', 'c', 1002, 0, 0),
 (42, '2020-11-30', 'c', 1004, 0, 0),
 (43, '2020-11-30', 'c', 1001, 0, 0),
 (44, '2020-12-27', 'c', 1005, 0, 0),
 (45, '2020-11-29', 'a', 1004, 0, 0),
 (46, '2020-11-30', 'a', 1004, 0, 0),
-(47, '2020-11-17', 'c', 1005, 33, 0),
+(47, '2020-11-17', 'c', 1005, 0, 0),
 (48, '2020-11-29', 'a', 1002, 33, 0),
 (49, '2020-12-01', 'a', 1003, 0, 0),
 (50, '2020-12-01', 'e', 1005, 0, 0);
@@ -373,6 +404,38 @@ INSERT INTO `roles` (`role_id`, `role`, `startdate`, `enddate`, `is_active`, `us
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `section_points`
+--
+
+CREATE TABLE `section_points` (
+  `point_id` int(255) NOT NULL,
+  `section_id` int(255) NOT NULL,
+  `lng` double NOT NULL,
+  `lat` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `section_points`
+--
+
+INSERT INTO `section_points` (`point_id`, `section_id`, `lng`, `lat`) VALUES
+(253, 55, 79.87600423851075, 6.8975350210569815),
+(254, 55, 79.85334493675231, 6.893061501727445),
+(255, 55, 79.85780813255269, 6.874229652918288),
+(256, 55, 79.87669088401759, 6.878532924038836),
+(257, 55, 79.87600423851075, 6.8975350210569815),
+(258, 56, 79.87603137715644, 6.89767032030521),
+(259, 56, 79.87642446460643, 6.878157800114906),
+(260, 56, 79.85787073692143, 6.874255199812083),
+(261, 56, 79.85975755668608, 6.860283628128101),
+(262, 56, 79.89065423033117, 6.867854865948402),
+(263, 56, 79.88758814821358, 6.906099480519174),
+(264, 56, 79.87453764484172, 6.906489715782158),
+(265, 56, 79.87603137715644, 6.89767032030521);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `stock_addition`
 --
 
@@ -382,6 +445,19 @@ CREATE TABLE `stock_addition` (
   `status` char(1) NOT NULL,
   `clerk_id` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `stock_addition`
+--
+
+INSERT INTO `stock_addition` (`sa_id`, `date`, `status`, `clerk_id`) VALUES
+(26, '2020-12-30', '0', 31),
+(27, '2020-12-30', '0', 31),
+(28, '2020-12-31', '0', 31),
+(29, '2020-12-31', '0', 31),
+(30, '2020-12-31', '0', 31),
+(31, '2020-12-31', '0', 31),
+(32, '2020-12-31', '0', 31);
 
 -- --------------------------------------------------------
 
@@ -394,6 +470,49 @@ CREATE TABLE `stock_addition_inventory_asc` (
   `item_id` int(255) NOT NULL,
   `quantity` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `stock_addition_inventory_asc`
+--
+
+INSERT INTO `stock_addition_inventory_asc` (`sa_id`, `item_id`, `quantity`) VALUES
+(26, 1, 3),
+(26, 7, 2),
+(26, 3, 10),
+(27, 5, 34),
+(27, 7, 3),
+(27, 9, 5),
+(27, 2, 4),
+(27, 10, 20),
+(28, 1, 45),
+(28, 7, 3),
+(28, 10, 20),
+(29, 4, 45),
+(30, 3, 100),
+(31, 8, 5),
+(31, 10, 10),
+(32, 2, 50),
+(32, 1, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tech_sections`
+--
+
+CREATE TABLE `tech_sections` (
+  `section_id` int(255) NOT NULL,
+  `tech_id` int(255) NOT NULL,
+  `color` varchar(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tech_sections`
+--
+
+INSERT INTO `tech_sections` (`section_id`, `tech_id`, `color`) VALUES
+(55, 33, '#fa0000'),
+(56, 35, '#2bff00');
 
 -- --------------------------------------------------------
 
@@ -444,9 +563,11 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`userId`, `username`, `password`, `Name`, `phone`, `occuFlag`, `statusFlag`) VALUES
 (0, 'default', 'default', 'default', '', 1, 0),
 (30, 'DS', '81dc9bdb52d04dc20036dbd8313ed055', 'Bappa', '+947015492', 1, 1),
-(31, 'Clerck', '81dc9bdb52d04dc20036dbd8313ed055', 'Lakshan Sandaruwan', '+947015492', 2, 1),
+(31, 'Clerck', '81dc9bdb52d04dc20036dbd8313ed055', 'Lakshan Jayasinghe', '+947015492', 2, 1),
 (32, 'Storekeeper', '81dc9bdb52d04dc20036dbd8313ed055', 'xxxxx', '+947015492', 3, 1),
-(33, 'Tech', '81dc9bdb52d04dc20036dbd8313ed055', 'tech bla', '+947015492', 4, 1);
+(33, 'Maldeniya', '81dc9bdb52d04dc20036dbd8313ed055', 'Maldeniya', '+947015492', 4, 1),
+(34, 'Tech', '81dc9bdb52d04dc20036dbd8313ed055', 'Tech1', '+947015492', 4, 1),
+(35, 'Isuru', '81dc9bdb52d04dc20036dbd8313ed055', 'Isuru', '+947015492', 4, 1);
 
 --
 -- Indexes for dumped tables
@@ -567,6 +688,13 @@ ALTER TABLE `roles`
   ADD KEY `userrole` (`user_id`);
 
 --
+-- Indexes for table `section_points`
+--
+ALTER TABLE `section_points`
+  ADD PRIMARY KEY (`point_id`),
+  ADD KEY `section` (`section_id`);
+
+--
 -- Indexes for table `stock_addition`
 --
 ALTER TABLE `stock_addition`
@@ -579,6 +707,13 @@ ALTER TABLE `stock_addition`
 ALTER TABLE `stock_addition_inventory_asc`
   ADD KEY `item_id` (`item_id`),
   ADD KEY `stock_addition_inventory_asc_ibfk_2` (`sa_id`);
+
+--
+-- Indexes for table `tech_sections`
+--
+ALTER TABLE `tech_sections`
+  ADD PRIMARY KEY (`section_id`),
+  ADD KEY `tech` (`tech_id`);
 
 --
 -- Indexes for table `tmpinventory`
@@ -602,13 +737,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `complainer`
 --
 ALTER TABLE `complainer`
-  MODIFY `complainer_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `complainer_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `complaint`
 --
 ALTER TABLE `complaint`
-  MODIFY `complaint_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `complaint_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `fraud`
@@ -632,7 +767,7 @@ ALTER TABLE `inventory`
 -- AUTO_INCREMENT for table `itemrequest`
 --
 ALTER TABLE `itemrequest`
-  MODIFY `Itemrequest_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+  MODIFY `Itemrequest_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 
 --
 -- AUTO_INCREMENT for table `newlamppostrecord`
@@ -650,7 +785,7 @@ ALTER TABLE `notification`
 -- AUTO_INCREMENT for table `repair`
 --
 ALTER TABLE `repair`
-  MODIFY `repair_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `repair_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT for table `repair_inventory_asc`
@@ -665,10 +800,22 @@ ALTER TABLE `roles`
   MODIFY `role_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
+-- AUTO_INCREMENT for table `section_points`
+--
+ALTER TABLE `section_points`
+  MODIFY `point_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=266;
+
+--
 -- AUTO_INCREMENT for table `stock_addition`
 --
 ALTER TABLE `stock_addition`
-  MODIFY `sa_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `sa_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT for table `tech_sections`
+--
+ALTER TABLE `tech_sections`
+  MODIFY `section_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT for table `tmpinventory`
@@ -680,7 +827,7 @@ ALTER TABLE `tmpinventory`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `userId` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `userId` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- Constraints for dumped tables
@@ -767,6 +914,12 @@ ALTER TABLE `roles`
   ADD CONSTRAINT `userrole` FOREIGN KEY (`user_id`) REFERENCES `user` (`userId`) ON DELETE CASCADE;
 
 --
+-- Constraints for table `section_points`
+--
+ALTER TABLE `section_points`
+  ADD CONSTRAINT `section` FOREIGN KEY (`section_id`) REFERENCES `tech_sections` (`section_id`) ON DELETE CASCADE;
+
+--
 -- Constraints for table `stock_addition`
 --
 ALTER TABLE `stock_addition`
@@ -778,6 +931,12 @@ ALTER TABLE `stock_addition`
 ALTER TABLE `stock_addition_inventory_asc`
   ADD CONSTRAINT `stock_addition_inventory_asc_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `inventory` (`Item_id`),
   ADD CONSTRAINT `stock_addition_inventory_asc_ibfk_2` FOREIGN KEY (`sa_id`) REFERENCES `stock_addition` (`sa_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `tech_sections`
+--
+ALTER TABLE `tech_sections`
+  ADD CONSTRAINT `tech` FOREIGN KEY (`tech_id`) REFERENCES `user` (`userId`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `tmpinventory`
