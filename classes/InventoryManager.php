97d9f5bb91df8ebe6e4d1679856dcbf9d9e7954b
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-02-08 09:37:47 +0530

    inventory functions

--- classes/InventoryManager.php
+++ classes/InventoryManager.php
@@ -1,10 +1,55 @@
 <?php
 namespace classes;
-// include_once '../utils/modelLoader.php';
 include_once  __DIR__ . '/../utils/classloader.php';
 
 class InventoryManager
 {
+
+    //--------------------------------- Inventory --------------------------------
+
+
+     // check is there inventory have enough items to reduce
+     public function CheckInventoryBeforeReduce($items)
+     {
+         $err = '';
+         $invmodel = new \models\Inventory();
+         foreach ($items as $item) {
+             if (!$invmodel->checkAvailability($item[0],$item[1])) {
+                 $err = $err.$item[2]." ,";
+             }
+         }
+         return $err;
+     }
+
+    // decrease quntities from inv when tech confirm the supplied itemrequst
+    public function DecreaseInventory($items)
+    {
+        $invmodel = new \models\Inventory();
+        foreach ($items as $item) {
+            $invmodel->updateQuantity($item[0],$item[1],'-');
+        }
+    }
+
+
+    // increase quntities from inv when when storekeeper confirm the stockaddition
+    public function IncreaseInventory($items)
+    {
+        $invmodel = new \models\Inventory();
+        foreach ($items as $item) {
+            $invmodel->updateQuantity($item[0],$item[1],'+');
+        }
+    }
+     
+
+
+
+
+
+
+    //--------------------------------- TmpInventory --------------------------------
+
+
+
 
     // create tmpinv when new tech account is created
     public function AddTmpInventoryfortech($tech)
@@ -39,8 +84,7 @@ class InventoryManager
         }
     }
 
-
-    // increase quntities from tmpinv when tech confim the supplied itemrequst
+    // increase quntities from inv when tech confim the supplied itemrequst
     public function IncreasetmpInventory($items,$tech_id)
     {
         $tmpmodel = new \models\TmpInventory();
@@ -48,23 +92,7 @@ class InventoryManager
             $tmpmodel->updateQuantity($tech_id,$item[0],$item[1],'+');
         }
     }
-        
-    // public function AddItems($tech,$items)
-    // {
-    //     $tmpmodel = new \models\TmpInventory();
-    //     foreach ($items as $item) {
-    //         $tmpmodel->updateQuantity($tech,$item[0],$item[1],'-');
-    //     }
-        
-    // }
-    // public function check($tech,$items)
-    // {
-    //     $tmpmodel = new \models\TmpInventory();
-    //     foreach ($items as $item) {
-    //         $tmpmodel->checkAvailability($tech,$item[0],$item[1]);
-    //     }
-        
-    // }
+
 
 
 
