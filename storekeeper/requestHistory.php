ec9a4ca0b9f8d4e300eb0c1885b9c5c5ae547069
Gajani1998 <gajanikangesan@gmail.com>
2021-02-09 11:36:00 +0530

    something changed

--- storekeeper/requestHistory.php
+++ storekeeper/requestHistory.php
@@ -12,6 +12,7 @@ $session = new classes\Session(StorekeeperFL);
     <link rel="stylesheet" href="../css/slider.css">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="./store.css">
+   
     <!-- <link rel="stylesheet" href="../Clerk/css/repairpage.css"> -->
     <script src="https://kit.fontawesome.com/2b554022ef.js" crossorigin="anonymous"></script>
     <title>Item Requests</title>
@@ -19,7 +20,8 @@ $session = new classes\Session(StorekeeperFL);
 </head>
 <body>
    
-    <?php include "./views/nav.php" ?>
+    <?php 
+    include "./views/nav.php" ?>
 
 
     
@@ -28,13 +30,16 @@ $session = new classes\Session(StorekeeperFL);
             <h1>Item Request History</h1>
         </header>
         <div class="container">
-            <div class="p-list-section sc-bar">
+           
+        <div class="p-list-section sc-bar">
 
 
-                <div class="xx">
-                    <h2>Request List</h2>
-                </div>
-            <?php
+<div class="xx">
+    <h2>Request List</h2>
+</div>
+
+<!-- repair list -->
+<?php
              $servername = "localhost";
              $username = "root";
              $password = "";
@@ -43,113 +48,26 @@ $session = new classes\Session(StorekeeperFL);
             
              $mysqli = new mysqli($servername, $username, $password, $dbname, $port);
 
-               $query = "SELECT supplied_date,username FROM itemrequest INNER JOIN user ON itemrequest.created_by=user.userid WHERE itemrequest.status='c'";
+               $query = "SELECT supplied_date,userId,username,itemrequest_id,added_date FROM itemrequest INNER JOIN user ON itemrequest.created_by=user.userId WHERE itemrequest.status='c'";
                if($result = $mysqli->query($query)){
                 while ($row = $result->fetch_assoc()) { ?>
                 <div id="" class="repair-item">
-                <div class="row">
-                                <span>Supplied Date: <?= $row['supplied_date'] ?></span>
-                                <span>Technician: <?= $row['username'] ?></span>
-                                <i class="s fas fa-check"></i>
+               
+                <div id="<?= $row['userId'] ?>" class="repair-item" onclick="location.href='<?= "./request.php?id=" . $row['userId']  ?>' ;">
+
+                <div class="row1"><span>#<?= $row["itemrequest_id"] ?></span>
+               <span><?= $row["supplied_date"] ?></span>
                 </div>
                 </div>
-                <?php 
-                }
+            </div>                            
+               
+             <?php }
                 
                 $result->free();
-             } 
-                ?>
-            </div>
-            <div class="table-section sc-bar">
-                    <div class="details">
-                        <h2>Request Details</h2>
-    
-                        <table class="content-table">
-    
-                            <tbody>
-                                <tr>
-                                    <td>Technician Name</td>
-                                    <td>Bla Bla</td>
-                                </tr>
-                                <tr>
-                                    <td>Requested Date</td>
-                                    <td>2020-10-14</td>
-                                </tr>
-                                <tr>
-                                    <td>Supplied Date</td>
-                                    <td>2020-10-15</td>
-                                </tr>
-                               
-    
-                            </tbody>
-    
-    
-                        </table>
-                    </div>
-
-                    <!-- supply items -->
-                    <div class="details">
-                        <h2 style="margin-bottom: 8px;"> Supplied Items</h2>
-                    <table class="content-table">
-                        <thead>
-                            <tr>
-                                <th>ITEM ID</th>
-                                <th>ITEM NAME</th>
-                                <th>QUANTITY</th>
-
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>LED BULB</td>
-                                <td>80</td>
-
-                            </tr>
-                            <tr>
-                                <td>1</td>
-                                <td>LED BULB</td>
-                                <td>80</td>
-
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>SUNBOXES</td>
-                                <td>20</td>
-
-                            </tr>
-                            <tr>
-                                <td>3</td>
-                                <td>WIRES</td>
-                                <td>50m</td>
-
-                            </tr>
-                            <tr>
-                                <td>4</td>
-                                <td>FUSE</td>
-                                <td>20</td>
-
-                            </tr>
-                            <tr>
-                                <td>5</td>
-                                <td>BULB</td>
-                                <td>80</td>
-
-                            </tr>
-                        
-
-                        </tbody>
+             } ?>
 
 
-                    </table>
-
-                    </div>
-                </div>
-
-
-            </div>
-        </div>
-
+</div>
                          
 
 </body>
