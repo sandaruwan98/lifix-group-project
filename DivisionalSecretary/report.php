ab1d8d7d02f2911b05f9f7642e520aecb3219973
UdithaIshan <uditha.ishan34@gmail.com>
2021-03-23 11:51:27 +0530

    queries updated

--- DivisionalSecretary/report.php
+++ DivisionalSecretary/report.php
@@ -4,124 +4,131 @@ use models\ReportGenerate;
 
 include_once  __DIR__ . '/../models/ReportGenerate.php';
 include_once  __DIR__ . '/../utils/classloader.php';
+
 $inventryModel = new models\Inventory();
+$usermodel = new models\User();
+$fraudmodel = new models\Fraud();
 
 $session = new classes\Session(DSFL);
 
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Report Generate</title>
-  <script src="https://kit.fontawesome.com/2b554022ef.js" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="../css/slider.css">
-  <link rel="stylesheet" href="./css/style.css">
-  <link rel="stylesheet" href="./css/report.css">
-  <link rel="stylesheet" href="./css/charts.css">
-  <link rel="stylesheet" href="../css/morris.css">
-  <script src="../js/jquery-3.5.1.min.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-  <script src="../js/morris.min.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Technician Report</title>
+    <script src="https://kit.fontawesome.com/2b554022ef.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../css/slider.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/report.css">
+    <link rel="stylesheet" href="./css/charts.css">
+    <link rel="stylesheet" href="../css/morris.css">
+    <script src="../js/jquery-3.5.1.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+    <script src="../js/morris.min.js"></script>
 </head>
 
 <body>
-
-  <?php include "../components/userfeild.php" ?>
-  <?php include "./nav.html" ?>
-
-  <div class="container">
-
-    <h1>Report Generate</h1>
-
-    <div class="top-bar">
-      <div class="range">
-        <p>From</p>
-        <input type="date" id="firstDate" class="field" required>
-        <p>To</p>
-        <input type="date" id="secondDate" class="field" required>
-        <button class="btn">Generate</button>
-      </div>
-    </div>
-
-    <div class="chart-row">
-
-      <div class="chart-column">
-        <div class="chart-title">
-          <h2>Recieved Complaints</h2>
-        </div>
-        <div class="chart-column-data">
-          <p class="chart-column-data-p">
-            No data to show
-          </p>
-        </div>
-      </div>
-
-      <div class="chart-column-middle"></div>
-
-      <div class="chart-column">
-        <div class="chart-title">
-          <h2>Current Inventory</h2>
+    <?php include "../components/userfeild.php" ?>
+    <?php include "./nav.html" ?>
+    <div class="container">
+        <h1>Report Generate</h1>
+        <div class="top-bar">
+            <div class="range">
+                <p>Check</p>
+                <?php
+                $result = $usermodel->getUsers(4);
+                ?>
+                <select name="useracc" id="useracc" class="field" required>
+                    <option value="" disabled selected>select account</option>
+                    <?php while ($row = $result->fetch_assoc()) : ?>
+                        <option value="<?= $row['Name'] ?>"><?= $row['Name'] ?></option>
+                    <?php endwhile ?>
+                </select>
+                <p>From</p>
+                <input type="date" id="firstDate" class="field" required>
+                <p>To</p>
+                <input type="date" id="secondDate" class="field" required>
+                <button class="btn">Generate</button>
+            </div>
         </div>
 
-        <?php
-        $inventry = $inventryModel->getInventory();
-        $data = "";
-        while ($row = mysqli_fetch_array($inventry)) {
-          $data .= "{ name:'" . $row["name"] . "',total:" . $row["total"] . "},";
-        }
-        ?>
+        <div class="chart-row" style="display: flex; align-items:center">
+            <div class="chart-column" style="height: 130px;">
+                <div class="chart-title">
+                    <h2>Technician's Bio</h2>
+                </div>
+                <div class="chart-column-data" style="height: 130px;">
 
-        <div class="chart-container">
-          <div id="myfirstchart" style="height:auto; width:auto">
-          </div>
-        </div>
-      </div>
+                    <p class="chart-column-data-p">
+                        No data to show
+                    </p>
+                </div>
+            </div>
 
-    </div>
-  </div>
+            <div class="chart-column-middle"></div>
+            <div class="chart-row" style="display: flex; align-items:center">
+                <div class="chart-column" style="max-height: 130px;">
+                    <div class="chart-title">
+                        <h2>Returned Items Mismatches</h2>
+                    </div>
+                    <div id="chart-column-data" class="chart-column-data" style="height: 130px; display: flex !important;">
+                        <p class="fraud-data chart-column-data-p">
+                            No data to show
+                        </p>
+                    </div>
 
+                </div>
+            </div </div>
 
-  <script>
+<script>
     $(document).ready(function() {
-      $('.btn').click(function() {
 
-        let date1 = $("#firstDate").val();
-        let date2 = $("#secondDate").val();
+        $('.btn').click(function() {
 
-        $.ajax({
-          type: "POST",
-          url: "../utils/reportFetch.php",
-          data: {
-            firstDate: date1,
-            secondDate: date2
-          },
-          success: function(data) {
-            if (data != "No data on this period") {
-              $(".chart-column-data").css({
-                "display": "block"
-              });
-              $(".chart-column-data-p").html(data);
-            } else
-              $(".chart-column-data-p").html(data);
-          }
-        });
-      })
+            let name = $("#useracc").val();
+            let date1 = $("#firstDate").val();
+            let date2 = $("#secondDate").val();
+
+            console.log(name);
+
+            $.ajax({
+                type: "POST",
+                url: "../utils/tecReportFetch.php",
+                data: {
+                    name: name,
+                    firstDate: date1,
+                    secondDate: date2
+                },
+                success: function(data) {
+                    data = JSON.parse(data);
+                    if (data.bio != "No data on this period") {
+                        $(".chart-column-data").css({
+                            "display": "block"
+                        });
+                        $("#chart-column-data").css({
+                            "display": "flex"
+                        });
+                        console.log(data)
+                        $(".chart-column-data-p").html(data.bio);
+                        $(".fraud-data").html(data.fraud);
+
+                    } else {
+                        $(".chart-column-data").css({
+                            "display": "flex"
+                        });
+                        $(".chart-column-data-p").html(data.bio);
+                        $(".fraud-data").html(data.fraud);
+                    }
+                }
+            });
+
+        })
     });
-
-    new Morris.Bar({
-      element: 'myfirstchart',
-      data: [<?php echo $data; ?>],
-      xkey: 'name',
-      ykeys: ['total'],
-      labels: ['Count']
-    });
-  </script>
-
+</script>
 </body>
 
 </html>
