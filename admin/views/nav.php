758636b04ea787922e1e5f7a3e51b49b6fa40c52
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-03-24 00:49:56 +0530

    admin class

---
+++ admin/views/nav.php
@@ -0,0 +1,22 @@
+<nav class="sidebar">
+    <!-- <h2 class="link-text">MENU</h2> -->
+    <ul>
+        <li class="nav-logo"><span class="nav-link" href="#"><i class="fas fa-lightbulb"></i><span class="link-text" style="margin-left: 5px;">LiFix</span> </span>
+        </li>
+        <!-- <li class="nav-item"><a class="nav-link" href="./index.php"><i class="fas fa-home"></i><span class="link-text">Home</span> </a></li> -->
+        <!-- <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-bell"></i><span class="link-text">Notifications</span> </a></li> -->
+        <li class="nav-item"><a class="nav-link" href="./index.php"><i class="fas fa-columns"></i><span class="link-text">Assign Sections</span> </a></li>
+        <li class="nav-item"><a class="nav-link" href="./t.php"><i class="fas fa-cog"></i><span class="link-text">User Control</span> </a></li>
+        <!-- <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-cog"></i><span class="link-text">Settings</span></a></li> -->
+        <li class="nav-item"><a class="nav-link" href="./../login/Logout.php"><i class="fas fa-sign-out-alt"></i><span class="link-text">LogOut</span></a></li>
+    </ul>
+</nav>
+
+
+<?php include "../components/notification.php" ?>
+<?php include "../components/toast.php" ?>
+<?php include "../components/userfeild.php" ?>
+<?php include '../components/noti_modals/modal_lp.php'; ?>
+
+<script src="../js/slider.js"></script>
+
