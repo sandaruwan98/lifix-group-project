2c638309fb7fd8a26aafc0287d4950a25c8629ae
UdithaIshan <uditha.ishan34@gmail.com>
2021-03-24 20:28:48 +0530

    confirmation msg added

--- utils/apiCaller.php
+++ utils/apiCaller.php
@@ -1,8 +1,11 @@
 <?php
+    // textBiz credentials
+    $user = "94701549225";
+    $password = "9221";
     
     $recipient = $_POST["phone"];
 
-    if($recipient) {
+    if($recipient && isset($_POST['from'])) {
         if (session_id() == ""){
             session_start();
         }
@@ -10,13 +13,34 @@
         $otp = rand(1000,9999);
         $_SESSION["otp"] = "$otp";
 
-        call($recipient, $otp);
+        sendOtp($recipient, $otp, $user, $password);
     }
 
-    function call($recipient, $otp) {
-        $user = "94701549225";
-        $password = "9221";
+    // call this when need to send an OTP
+    function sendOtp($recipient, $otp, $user, $password) {
+
         $text = urlencode("Your Li-Fix OTP is $otp");
+        $to = $recipient;
+        
+        $baseurl ="http://www.textit.biz/sendmsg";
+        $url = "$baseurl/?id=$user&pw=$password&to=$to&text=$text";
+        $ret = file($url);
+        
+        $res= explode(":",$ret[0]);
+        
+        if (trim($res[0])=="OK")
+        {
+        echo "Sent";
+        }
+        else
+        {
+        echo "Not Sent";
+        }
+    }
+
+    // call this when need to send comfirmation msg
+    function sendConfirmation($recipient, $user, $password) {
+        $text = "Successful";
         $to = $recipient;
         
         $baseurl ="http://www.textit.biz/sendmsg";
