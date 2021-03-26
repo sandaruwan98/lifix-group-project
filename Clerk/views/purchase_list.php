f1d136eb32d34d249cdb708d6ca6fbb58ab50a8b
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-03-26 14:35:27 +0530

    search functinality for reparhistory and purchese page

--- Clerk/views/purchase_list.php
+++ Clerk/views/purchase_list.php
@@ -2,11 +2,22 @@
 
     <button id="btnAdd" class="btn">ADD NEW</button>
     <!-- searchbox -->
-    <div class="searchbox-sm">
-        <input id="search" type="text" placeholder="Search..." name="search" class="search" style="font-size: small;">
-        <button class="btn-search"><i class="fas fa-search"></i></button>
-    </div>
+    <form action="purchase.php" method="post">
 
+        <div class="searchbox-sm">
+            <input name="searchbox" id="search" type="text" placeholder="Search..." name="search" class="search" style="font-size: small;">
+            <button name="search" class="btn-search"><i class="fas fa-search"></i></button>
+        </div>
+        <?php if( isset($_SESSION["psearch"]) ): ?>
+
+        <div class="sdisplay">
+            <p>Search Results for "<?= $_SESSION["psearch"] ?>"</p>
+            <button name="clearsearch" type="submit" class="btn btn-search "><i class="fas fa-times"> Clear Search</i></button>
+
+        </div>
+        <?php endif ?>
+
+    </form>
 
     <!-- repair list -->
     <?php
@@ -16,9 +27,14 @@
     ?>
         <div id="<?= $row['sa_id'] ?>" class="repair-item">
             <div class="row">
+            <div>
                 <span>#<?= $row['sa_id'] ?></span>
+                <span><?= $row['Name'] ?></span>
+            </div>
+            <div>
                 <span><?= $row['date'] ?></span>
                 <i class="s fas fa-check"></i>
+            </div>
             </div>
         </div>
     <?php endwhile ?>
