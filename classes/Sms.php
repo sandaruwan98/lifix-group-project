cd6c4c89e556dc23bfdf1f9aad52fd3c4ed3e787
UdithaIshan <uditha.ishan34@gmail.com>
2021-03-27 22:17:41 +0530

    sms functions now in sms class

--- classes/Sms.php
+++ classes/Sms.php
@@ -2,50 +2,48 @@
 
 namespace classes;
 
-class Sms {
+class Sms
+{
+
+    //textBiz credentials
     public static $user = "94701549225";
     public static $password = "9221";
 
-    public static function sendOtp($recipient, $otp) {
+    public static function sendOtp($recipient, $otp)
+    {
 
         $text = urlencode("Your Li-Fix OTP is $otp");
         $to = $recipient;
-        
-        $baseurl ="http://www.textit.biz/sendmsg";
-        $url = $baseurl."/?id=".self::$user."&pw=".self::$password."&to=".$to."&text=".$text;
+
+        $baseurl = "http://www.textit.biz/sendmsg";
+        $url = $baseurl . "/?id=" . self::$user . "&pw=" . self::$password . "&to=" . $to . "&text=" . $text;
         $ret = file($url);
-        
-        $res= explode(":",$ret[0]);
-        
-        if (trim($res[0])=="OK")
-        {
+
+        $res = explode(":", $ret[0]);
+
+        if (trim($res[0]) == "OK") {
             echo "Sent";
-        }
-        else
-        {
+        } else {
             echo "Not Sent";
         }
     }
 
     // call this when need to send comfirmation msg
-    public static function sendConfirmation($recipient, $lamp_id) {
+    public static function sendConfirmation($recipient, $lamp_id)
+    {
         $text = "Your complaint on lamppost:$lamp_id is now repaired";
         $to = $recipient;
-        
-        $baseurl ="http://www.textit.biz/sendmsg";
-        $url = $baseurl."/?id=".self::$user."&pw=".self::$password."&to=".$to."&text=".$text;
+
+        $baseurl = "http://www.textit.biz/sendmsg";
+        $url = $baseurl . "/?id=" . self::$user . "&pw=" . self::$password . "&to=" . $to . "&text=" . $text;
         $ret = file($url);
-        
-        $res= explode(":",$ret[0]);
-        
-        if (trim($res[0])=="OK")
-        {
-        echo "Sent";
-        }
-        else
-        {
+
+        $res = explode(":", $ret[0]);
+
+        if (trim($res[0]) == "OK") {
+            echo "Sent";
+        } else {
             echo "Not Sent";
         }
     }
-    
 }
